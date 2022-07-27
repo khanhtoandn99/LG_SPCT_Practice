e7129254d083351b2f53b3d3e9eaacdeb3e794b6
TOAN VO KHANH NGUYEN/LGEVH VS FUNCTIONAL TECHNOLOGY 4(toan4.nguyen@lge.com) <toan4.nguyen@lge.com>
2022-07-27 14:40:22 +0700

    test3.cpp (1) in-progress 20%

--- SCPT_pretest/test3.cpp
+++ SCPT_pretest/test3.cpp
@@ -12,7 +12,7 @@ void InputData() {
     cin >> B >> S >> D;
 }
 
-int chtoint(char x)
+int chtoi(char x)
 {
     return ('0' <= x && x <= '9') ? (int)(x - 48) : (int)(x - 65 + 10);
 }
@@ -29,32 +29,52 @@ void Solve()
         return;
     }
 
-    bool bSNegative = (S[0] == '-') ? true : false;
-    bool bDNegative = (D[0] == '-') ? true : false;
+    bool bSNegative = false;
+    bool bDNegative = false;
     int iSLen = strlen(S);
     int iDLen = strlen(D);
 
-    int vRes[iDLen - bDNegative][iSLen - bSNegative + iDLen - bDNegative - 1];
+    if (B == 10 && S[0] == '-') {
+        bSNegative = true;
+        iSLen -= 1;
+        for (int i = 0; i < iSLen; ++i) {
+            S[i] = S[i + 1];
+        }
+        S[iSLen] = 0;
+    }
+
+    if (B == 10 && D[0] == '-') {
+        bDNegative = true;
+        iDLen -= 1;
+        for (int i = 0; i < iDLen; ++i) {
+            D[i] = D[i + 1];
+        }
+        D[iDLen] = 0;
+    }
+
+    int vResLen = iSLen + iDLen - 1;
+    int vRes[iDLen][vResLen];
     memset(vRes, 0x0, sizeof(vRes));
 
-    for (int i = iDLen - 1; i >= bDNegative; --i) {
-        for (int j = iSLen - 1; j >= bSNegative; --j) {
-            vRes[i - bDNegative][iSLen - 1 - j + iDLen - 1 - i] = chtoint(D[i])*chtoint(S[j]);
+    for (int i = iDLen - 1; i >= 0; --i) {
+        for (int j = iSLen - 1; j >= 0; --j) {
+            vRes[i][iSLen - 1 - j + (iDLen - 1 - i)] = chtoi(D[i])*chtoi(S[j]);
         }
     }
 
-    char res[iSLen - bSNegative + iDLen - bDNegative]{0};
+    int iResLen = iSLen + iDLen;
+    char res[iResLen]{0};
     int iRem = 0;
-    for (int i = 0; i < iSLen - bSNegative + iDLen - bDNegative - 1; ++i) {
+    for (int i = 0; i < vResLen; ++i) {
         int iSum = 0;
-        for (int j = 0; j < iDLen - bDNegative; ++j) {
+        for (int j = 0; j < iDLen; ++j) {
             iSum += vRes[j][i];
         }
         res[i] = itoch((iSum + iRem)%B);
         iRem = (int)(iSum + iRem)/B;
         // cout << res[i] << " " << iRem << " -- ";
     }
-    if (iRem > 0) res[strlen(res)] = itoch(iRem);
+    if (iRem > 0) res[iResLen - 1] = itoch(iRem);
 
     if (bSNegative ^ bDNegative) cout << "-";
     for (int i = strlen(res) - 1; i >= 0; --i)
