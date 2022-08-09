bf413e64797d84965be5f692a39c56e4758cb2a6
Nguyen Vo Khanh Toan <admin@Apples-MacBook-Air.local>
2022-08-09 21:19:44 +0700

    test5.cpp resolved

--- SCPT_pretest/test5.cpp
+++ SCPT_pretest/test5.cpp
@@ -1,67 +1,49 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
 int N;//Number of mushrooms
 int P[150000 + 10];//Mushroom value
 
-int posIdx[150000];
-
-void InputData(){
-	cin >> N;
-	for (int i = 0; i < N; i++){
-		cin >> P[i];
-        posIdx[i] = -1;
-	}
+void InputData() {
+    cin >> N;
+    for (int i = 0; i < N; i++){
+       cin >> P[i];
+    }
 }
 
-bool myfunction (int i,int j) { return (i<j); }
+int main() { 
+    int ans = -1;
 
-int main(){
-	int ans = -1;
+    InputData();			//	Input function
 
-	InputData(); //	Input function
+    //	Write the code
+    /**
+     * Use Greedy Algorithm:
+     * Find descreasing subsequences one by one
+     * Then let First Element value subtract Last Element (max - min)
+     * Plus to iSum variable
+     * Finally, plus the P[N-1] in 
+     * case Last Element of most recent equal to N - 1 or not.
+     */
+    int iSum = 0;
+    int iPLastIdx = 0;
+    for (int i = 0; i < N - 1; ++i) {
+        if (P[i] <= P[i+1]) continue;
 
-	//	Write the code
-    int sum = 0;
-    int iPos = 0;
-    for (int i = 0; i < N; i += 2) {
-        if (P[i] > P[i+1] && P[i+1] > P[i+2] ||
-            P[i] <= P[i+1] && P[i+1] < P[i+2]) {
-            posIdx[iPos++] = i;
-            sum += P[i];
+        int iLen = 2;
+        for (int j = i + 1; j < N - 1; ++j) {
+            if (P[j] > P[j+1]) ++iLen;
+            else break;
         }
-        else if (P[i] <= P[i+1] && P[i+1] > P[i+2]) {
-            posIdx[iPos++] = i + 1;
-            sum += P[i+1];
-        }
+
+        iSum += (P[i] - P[i + iLen - 1]);
+        // cout << "iSum = " << iSum << endl;
+        iPLastIdx = i + iLen - 1;
+
+        i += iLen - 1;
     }
 
-    // // debug:
-    // for (int i = 0; posIdx[i] != -1; ++i) {
-    //     cout << P[posIdx[i]] << " ";
-    // }
-    // cout << endl;
-
-    // posIdx[] = {0, 3, 7};
-    // 0   1   2   3   4   5   6   7
-    // 7   2   1   8   4   3   5   6
-    // 0   -   -   3   -   -   -   7
-
-    cout << "sum = " << sum << endl;
-    for (int i = 0; i < iPos; ++i) {
-        int *pBegin = P, iLen = 0;
-        pBegin = pBegin + posIdx[i] + 1;
-        if (posIdx[i+1] != -1) iLen = posIdx[i+1] - posIdx[i];
-
-        std::sort(pBegin, pBegin + iLen);
-
-        if (posIdx[i+1] == -1) sum -= 0;
-        else sum -= pBegin[0];
+    ans = iSum + P[N-1];
+    cout << ans << endl;	//	Output answer
+    return 0;
     }
-
-    ans = sum;
-	cout << ans << endl;	//	Output answer
-	return 0;
-}
