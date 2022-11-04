26bc84e222edb85f4e642050b52478a60eeca04d
Nguyen Vo Khanh Toan <admin@Apples-MacBook-Air.local>
2022-11-05 05:47:43 +0700

    test10.cpp refactoring

--- SCPT_pretest/test10.cpp
+++ SCPT_pretest/test10.cpp
@@ -1,4 +1,79 @@
 #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+const int r[4] = {-1,0,1,0};
+const int c[4] = {0,1,0,-1};
+int N, M; // N: n.o Row, M: n.o Col
+int orgmap[51][51];
+bool visited[51][51] = {0,0};
+
+typedef struct
+{
+    int row;
+    int col;
+} POINT_T;
+
+vector<POINT_T> BFS(POINT_T s)
+{
+    vector<POINT_T> vPoint;
+    queue<POINT_T> Q;
+    Q.push(s);
+    visited[s.row][s.col] = true;
+    vPoint.push_back(s);
+    while (Q.size() != 0)
+    {
+        POINT_T curPoint = Q.front();
+        Q.pop();
+        for (int i = 0; i < 4; ++i) {
+            POINT_T p = {curPoint.row + r[i], curPoint.col + c[i]};
+            if (p.row < 0 || p.row > N-1) continue;
+            if (p.col < 0 || p.col > M-1) continue;
+            if (visited[p.row][p.col] == false && orgmap[p.row][p.col] == 1) {
+                Q.push(p);
+                visited[p.row][p.col] = true;
+                vPoint.push_back(p);
+            }
+        }
+    }
+    return vPoint;
+}
+
+int main()
+{
+    cin >> N >> M;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cin >> orgmap[i][j];
+        }
+    }
+    
+    vector<POINT_T> L1;
+    vector<POINT_T> L2;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            if (orgmap[i][j] == 1 && visited[i][j] == false) {
+                if (L1.size() == 0) {
+                    L1 = BFS({i,j});
+                    i = 0; j = 0; // re-scan to find L2
+                } else { // x = 2
+                    L2 = BFS({i,j});
+                }
+            }
+        }
+    }
+
+    int minPath = INT_MAX;
+    for (unsigned int i = 0; i < L1.size(); ++i) {
+        for (unsigned int j = 0; j < L2.size(); ++j) {
+            minPath = min(minPath, abs(L2[j].row - L1[i].row) + abs(L2[j].col - L1[i].col) - 1);
+        }
+    }
+    cout << minPath << endl;
+}
+
+/*
+#include <iostream>
 #include <algorithm>
 #include <vector>
 #include <queue>
@@ -77,3 +152,4 @@ int main()
     }
     cout << minDist << endl;
 }
+*/
