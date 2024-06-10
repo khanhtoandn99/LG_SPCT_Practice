e831c21534d0a40961aa9d26b2268af6f46bacd1
Nguyen Vo Khanh Toan <52823704+khanhtoandn99@users.noreply.github.com>
2024-06-10 17:41:15 +0700

    Update test1.cpp

--- SPCT_Pre-test/test1.cpp
+++ SPCT_Pre-test/test1.cpp
@@ -1,49 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int N;//Number of sponsored goods
-int D[100000 + 10];//Preference
-int sol = -30001;//Maximum preference of the first method
+int N;
+int D[100000 + 10] = {0,};
+int ans = INT_MIN;
 
-void InputData(){
-	cin >> N;
-	for(int i=0;i<N;i++){
-		cin >> D[i];
-	}
-}
-
-void Solve()
-{
-	int i, sum = 0;
-	for (i = 0; i < N - 2; i++) {
-		sum = D[i] + D[i + 1] + D[i + 2];
-		if (sum > sol) sol = sum;
-	}
-}
-
-void Solve2()
-{
-    for (int i = 0; i < N; i++) {
-        int k_max = N - i;
-        cout << "k_max = " << k_max << endl;
-        for (int k = 0; k < k_max; k++) {
-            int sum = 0;
-            for (int j = 0; j < k + 1; j++) {
-                cout << D[i + j] << " ";
-                sum += D[i + j];
-            }
-            cout << "sum = " << sum << endl;
-            if (sum > sol) sol = sum;
-        }
-        cout << endl << endl;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    
+    int dp[100000+10] = {0,};
+    cin >> D[0];
+    dp[0] = D[0];
+    ans = max(ans, dp[0]);
+    for (int i = 1; i < N; ++i) {
+        cin >> D[i];
+        dp[i] = max(D[i], dp[i-1]+D[i]);
+        ans = max(ans, dp[i]);
     }
-}
-
-
-int main(){
-	InputData(); // Input function
-	Solve2();
-	cout << sol << endl; // Output
-    system("pause");
-	return 0;
+    cout << ans << "\n";
+    return 0;
 }
